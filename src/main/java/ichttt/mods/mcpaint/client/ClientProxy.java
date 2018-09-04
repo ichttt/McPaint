08cb3f2dbff962e9a844a1457be0110f092eb063
Tobias Hotz <tobias.hotz@hotmail.de>
2018-09-04 13:27:21 +0200

    Remove unnecessary log notice, no one will read it anyways. Let's try and solve it the proper way

--- src/main/java/ichttt/mods/mcpaint/client/ClientProxy.java
+++ src/main/java/ichttt/mods/mcpaint/client/ClientProxy.java
@@ -16,7 +16,6 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 
 import java.util.List;
@@ -29,12 +28,6 @@ public class ClientProxy implements IProxy {
         MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
         EventHandler.STAMP.setTileEntityItemStackRenderer(TEISRStamp.INSTANCE);
         EventHandler.STAMP.addPropertyOverride(new ResourceLocation(MCPaint.MODID, "shift"), TEISRStamp.INSTANCE);
-        if (FMLClientHandler.instance().hasOptifine()) {
-            MCPaint.LOGGER.warn("OPTIFINE PRESENT");
-            MCPaint.LOGGER.warn("Some things may not render correctly");
-            MCPaint.LOGGER.warn("Especially shaders are known to cause issues");
-            MCPaint.LOGGER.warn("Please DO NOT report any render errors with optifine to MCPaint, but report them to Optifine instead");
-        }
     }
 
     @Override
