35f5777f6f29983cada870cb4cf99d38f66bd1d9
Tobias Hotz <tobias.hotz@hotmail.de>
2022-09-06 22:58:39 +0200

    More delegations

--- src/main/java/ichttt/mods/mcpaint/common/block/BlockCanvas.java
+++ src/main/java/ichttt/mods/mcpaint/common/block/BlockCanvas.java
@@ -1,5 +1,8 @@
 package ichttt.mods.mcpaint.common.block;
 
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.*;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
@@ -17,10 +20,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraft.world.level.Explosion;
-import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.Level;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -30,7 +29,6 @@ public class BlockCanvas extends Block implements EntityBlock {
     public static final BooleanProperty SOLID = BooleanProperty.create("solid");
     public static final BooleanProperty NORMAL_CUBE = BooleanProperty.create("normal_cube");
 
-    //TODO register a block for each common material
     public BlockCanvas(Material material) {
         super(Block.Properties.of(material).strength(1F, 4F).isRedstoneConductor((state, world, pos) -> state.getValue(NORMAL_CUBE)));
         registerDefaultState(stateDefinition.any().setValue(SOLID, true).setValue(NORMAL_CUBE, true));
@@ -138,18 +136,6 @@ public class BlockCanvas extends Block implements EntityBlock {
         return super.getVisualShape(state, world, pos, context);
     }
 
-
-
-//    @SuppressWarnings("deprecation")
-//    @Override
-//    public MaterialColor getMaterialColor(BlockState state, IBlockReader world, BlockPos pos) {
-//        TileEntityCanvas canvas = (TileEntityCanvas) world.getTileEntity(pos);
-//        if (canvas != null && canvas.getContainedState() != null) {
-//            return canvas.getContainedState().getMaterialColor(world, pos);
-//        }
-//        return super.getMaterialColor(state, world, pos);
-//    }
-
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         builder.add(SOLID, NORMAL_CUBE);
@@ -167,9 +153,88 @@ public class BlockCanvas extends Block implements EntityBlock {
     }
 
     @Override
-    public SoundType getSoundType(BlockState state) {
-        return super.getSoundType(state);
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().propagatesSkylightDown(canvas.getContainedState(), world, pos);
+        }
+        return super.propagatesSkylightDown(state, world, pos);
     }
+
+    @Override
+    public void animateTick(BlockState state, Level world, BlockPos pos, RandomSource random) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            state.getBlock().animateTick(canvas.getContainedState(), world, pos, random);
+            return;
+        }
+        super.animateTick(state, world, pos, random);
+    }
+
+    @Override
+    public int getLightEmission(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().getLightEmission(canvas.getContainedState(), world, pos);
+        }
+        return super.getLightEmission(state, world, pos);
+    }
+
+    @Override
+    public void fallOn(Level world, BlockState state, BlockPos pos, Entity entity, float pFallDistance) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            state.getBlock().fallOn(world, canvas.getContainedState(), pos, entity, pFallDistance);
+            return;
+        }
+        super.fallOn(world, state, pos, entity, pFallDistance);
+    }
+
+    @Override
+    public boolean isCollisionShapeFullBlock(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().isCollisionShapeFullBlock(canvas.getContainedState(), world, pos);
+        }
+        return super.isCollisionShapeFullBlock(state, world, pos);
+    }
+
+    @Override
+    public VoxelShape getBlockSupportShape(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().getBlockSupportShape(canvas.getContainedState(), world, pos);
+        }
+        return super.getBlockSupportShape(state, world, pos);
+    }
+
+    @Override
+    public boolean isOcclusionShapeFullBlock(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().isOcclusionShapeFullBlock(canvas.getContainedState(), world, pos);
+        }
+        return super.isOcclusionShapeFullBlock(state, world, pos);
+    }
+
+    @Override
+    public VoxelShape getInteractionShape(BlockState state, BlockGetter world, BlockPos pos) {
+        TileEntityCanvas canvas = (TileEntityCanvas) world.getBlockEntity(pos);
+        if (canvas != null && canvas.getContainedState() != null) {
+            state = canvas.getContainedState();
+            return state.getBlock().getInteractionShape(canvas.getContainedState(), world, pos);
+        }
+        return super.getInteractionShape(state, world, pos);
+    }
+
+
 
     public BlockState getStateFrom(BlockGetter world, BlockPos pos, BlockState state) {
         return defaultBlockState().setValue(SOLID, state.canOcclude()).setValue(NORMAL_CUBE, state.isRedstoneConductor(world, pos));
